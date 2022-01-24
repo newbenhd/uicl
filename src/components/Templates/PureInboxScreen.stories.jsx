6566c958aa6d74da93deb3e557631a60d9cc656a
Benjamin Morrison <newben.hd@gmail.com>
2022-01-24 08:35:40 -0800

    chromatic & github action setup

---
+++ src/components/Templates/PureInboxScreen.stories.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { fireEvent, within } from '@storybook/testing-library';
+
+import { PureInboxScreen } from './InboxScreen';
+
+import * as TaskListStories from '../Molecules/TaskList/TaskList.stories';
+
+// A super-simple mock of a redux store
+const Mockstore = configureStore({
+  reducer: {
+    tasks: createSlice({
+      name: 'tasks',
+      initialState: TaskListStories.Default.args.tasks,
+      reducers: {
+        updateTaskState: (state, action) => {
+          const { id, newTaskState } = action.payload;
+          const task = state.findIndex((task) => task.id === id);
+          if (task >= 0) {
+            state[task].state = newTaskState;
+          }
+        },
+      },
+    }).reducer,
+  },
+});
+
+export default {
+  component: PureInboxScreen,
+  title: 'PureInboxScreen',
+  decorators: [story => <Provider store={Mockstore}>{story()}</Provider>],
+};
+
+const Template = args => <PureInboxScreen {...args} />;
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = {
+  error: 'Something',
+};
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+const canvas = within(canvasElement);
+// Simulates pinning the first task
+await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+// Simulates pinning the third task
+await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+};
--- src/lib/store.js
+++ src/lib/store.js
@@ -3,15 +3,28 @@
  */
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
+// Our new error field is configured here
+const AppStateSlice = createSlice({
+    name: "appState",
+    initialState: "",
+    reducers: {
+        updateAppState: (state, action) => {
+            return {
+                ...state,
+                isError: action.payload,
+            };
+        },
+    },
+});
 /*
  * The initial state of our store when the app loads.
  * Usually, you would fetch this from a server.
  */
 const defaultTasks = [
-  { id: '1', title: 'Something', state: 'TASK_INBOX' },
-  { id: '2', title: 'Something more', state: 'TASK_INBOX' },
-  { id: '3', title: 'Something else', state: 'TASK_INBOX' },
-  { id: '4', title: 'Something again', state: 'TASK_INBOX' },
+    { id: '1', title: 'Something', state: 'TASK_INBOX' },
+    { id: '2', title: 'Something more', state: 'TASK_INBOX' },
+    { id: '3', title: 'Something else', state: 'TASK_INBOX' },
+    { id: '4', title: 'Something again', state: 'TASK_INBOX' },
 ];
 
 /*
@@ -20,18 +33,21 @@ const defaultTasks = [
  * https://redux-toolkit.js.org/api/createSlice
  */
 const TasksSlice = createSlice({
-  name: 'tasks',
-  initialState: defaultTasks,
-  reducers: {
-    updateTaskState: (state, action) => {
-      const { id, newTaskState } = action.payload;
-      const task = state.findIndex(task => task.id === id);
-      if (task >= 0) {
-        state[task].state = newTaskState;
-      }
+    name: 'tasks',
+    initialState: defaultTasks,
+    reducers: {
+        updateTaskState: (state, action) => {
+            const { id, newTaskState } = action.payload;
+            const task = state.findIndex(task => task.id === id);
+            if (task >= 0) {
+                state[task].state = newTaskState;
+            }
+        },
     },
-  },
 });
+
+// The actions contained in the new slice are exported to be used in our components
+export const { updateAppState } = AppStateSlice.actions;
 
 // The actions contained in the slice are exported for usage in our components
 export const { updateTaskState } = TasksSlice.actions;
@@ -42,9 +58,10 @@ export const { updateTaskState } = TasksSlice.actions;
  * https://redux-toolkit.js.org/api/configureStore
  */
 const store = configureStore({
-  reducer: {
-    tasks: TasksSlice.reducer,
-  },
+    reducer: {
+        tasks: TasksSlice.reducer,
+        isError: AppStateSlice.reducer,
+    },
 });
 
 export default store;
